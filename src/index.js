749c9a40bdb6b24bdfb96ada892519703bcb6d86
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-05 15:47:00 +0300

    refactor: correct if statement

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ module.exports = function check(str, bracketsConfig) {
     let bracketsOpening = [];
     let bracketsClosing = [];
     let bracketsStack = [];
+    
     bracketsConfig.forEach(element => {
         bracketsOpening.push(element[0]);
         bracketsClosing.push(element[1]);
@@ -15,17 +16,18 @@ module.exports = function check(str, bracketsConfig) {
     for (let i = 0; i < str.length; i++) {
         // if current bracket is an opening one put it into stack
         // in case opening and closing brackets are the same
-        // check if such bracket is already in the stack then current one is not an opening bracket
-        if (bracketsOpening.includes(str[i]) && !bracketsStack.includes(str[i])) {
+        // check if such bracket is already in the stack and
+        // then current one is not an opening bracket
+        if (bracketsOpening.includes(str[i]) && (!bracketsStack.includes(str[i]) || !bracketsClosing.includes(str[i]))) {
             bracketsStack.push(str[i]);
         } else if ((bracketsClosing.includes(str[i])) && (bracketsStack.length !== 0)){
             // check if current closing bracket matches last opening bracket
             // comparing opening bracket with respective index with opening bracket in the stack 
             // then deleting the opening bracket from the stack
-            if ((bracketsStack.length !== 0) && (bracketsStack[bracketsStack.length - 1] === bracketsOpening[bracketsClosing.indexOf(str[i])])) {
+            if (bracketsStack[bracketsStack.length - 1] === bracketsOpening[bracketsClosing.indexOf(str[i])]) {
                 bracketsStack.pop();
-            /*} else {
-                return false;*/
+            } else {
+                return false;
             }
         // if current bracket is a closing one and there is nothing to match return false
         } else if ((bracketsClosing.includes(str[i])) && (bracketsStack.length === 0)) {
