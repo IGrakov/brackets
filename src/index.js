2fbd4b90a41b57121033473c8b6730ff23acbf60
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-05 10:23:55 +0300

    feat: add full functionality of brackets checking

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length === 0 || bracketsConfig == 0) {
+        return true;
+    }
+
+    let bracketsOpening = [];
+    let bracketsClosing = [];
+    let bracketsStack = [];
+    bracketsConfig.forEach(element => {
+        bracketsOpening.push(element[0]);
+        bracketsClosing.push(element[1]);
+    });
+
+    str.split('').forEach(element => {
+        if (bracketsOpening.includes(element)) {
+            bracketsStack.push(element);
+        } else if (bracketsClosing.includes(element)){
+            // check if current closing bracket matches last opening bracket
+            // comparing opening bracket with respective index with opening bracket in the stack 
+            // then deleting the opening bracket from the stack
+            if (bracketsStack.length !== 0 && 
+                bracketsOpening[bracketsOpening.length - 1] === bracketsOpening[bracketsClosing.indexOf(element)]) {
+                bracketsStack.pop();
+            } else {
+                return false;
+            }
+        }
+    });
+
+    return bracketsStack.length === 0 ? true : false;
+
 }
