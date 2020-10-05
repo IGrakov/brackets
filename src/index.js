c797f31cca66cb6cb226ed6b1f1a13627a1a9892
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-05 13:07:07 +0300

    refactor: correct algorithm

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ module.exports = function check(str, bracketsConfig) {
     });
 
     for (let i = 0; i < str.length; i++) {
-        // if next is a closing bracket and there is nothing to match return false
-        if ((bracketsClosing.includes(str[i])) && (bracketsStack.length === 0)){
-            return false;
-        } else if (bracketsOpening.includes(str[i]) && !bracketsStack.includes(str[i])) {
+        // if current bracket is an opening one put it into stack
+        // in case opening and closing brackets are the same
+        // check if such bracket is already in the stack then current one is not an opening bracket
+        if (bracketsOpening.includes(str[i]) && !bracketsStack.includes(str[i])) {
             bracketsStack.push(str[i]);
         } else if ((bracketsClosing.includes(str[i])) && (bracketsStack.length !== 0)){
             // check if current closing bracket matches last opening bracket
@@ -24,9 +24,12 @@ module.exports = function check(str, bracketsConfig) {
             // then deleting the opening bracket from the stack
             if ((bracketsStack.length !== 0) && (bracketsStack[bracketsStack.length - 1] === bracketsOpening[bracketsClosing.indexOf(str[i])])) {
                 bracketsStack.pop();
-            } else {
-                return false;
+            /*} else {
+                return false;*/
             }
+        // if current bracket is a closing one and there is nothing to match return false
+        } else if ((bracketsClosing.includes(str[i])) && (bracketsStack.length === 0)) {
+            return false;
         }
     }
 
